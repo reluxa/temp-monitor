525b62c362267ada1611be6a3cf0e53a5c2b2f72
Peter Bagrij <Peter_Bagrij@epam.com>
2017-06-02 13:37:18 +0200

    captive

--- AutoConnect-deepsleep.ino
+++ AutoConnect-deepsleep.ino
@@ -26,6 +26,8 @@ char mqtt_user[40];
 char mqtt_password[40];
 char mqtt_topic[40];
 char blynk_token[34] = "YOUR_BLYNK_TOKEN";
+char captive_user[40];
+char captive_password[40];
 
 void doMeasurement() {
   while (!tempsensor.begin()) {
@@ -89,7 +91,7 @@ void reloadConfiguration() {
           configFile.readBytes(buf.get(), size);
           DynamicJsonBuffer jsonBuffer;
           JsonObject& json = jsonBuffer.parseObject(buf.get());
-          json.printTo(Serial);
+          //json.printTo(Serial);
           if (json.success()) {
             Serial.println("\nparsed json");
   
@@ -97,10 +99,10 @@ void reloadConfiguration() {
             strcpy(mqtt_port, json["mqtt_port"]);
             strcpy(mqtt_user, json["mqtt_user"]);
             strcpy(mqtt_password, json["mqtt_password"]);
-            strcpy(mqtt_topic, json["mqtt_topic"]);
-            
+            strcpy(mqtt_topic, json["mqtt_topic"]);            
             strcpy(blynk_token, json["blynk_token"]);
-  
+            strcpy(captive_user, json["captive_user"]);            
+            strcpy(captive_password, json["captive_password"]);
           } else {
             Serial.println("failed to load json config");
           }
@@ -132,6 +134,9 @@ void configPortal() {
     WiFiManagerParameter custom_mqtt_password("password", "mqtt password", mqtt_password, 40);
     WiFiManagerParameter custom_mqtt_topic("topic", "mqtt topic", mqtt_topic, 40);
     WiFiManagerParameter custom_blynk_token("blynk", "blynk token", blynk_token, 34);
+    WiFiManagerParameter custom_captive_user("captiveuser", "captive user", captive_user, 40);
+    WiFiManagerParameter custom_captive_password("captivepassword", "captive password", captive_password, 40);
+
    
     WiFiManager wifiManager;
     wifiManager.setSaveConfigCallback(saveConfigCallback);
@@ -141,6 +146,9 @@ void configPortal() {
     wifiManager.addParameter(&custom_mqtt_password);
     wifiManager.addParameter(&custom_mqtt_topic);
     wifiManager.addParameter(&custom_blynk_token);
+    wifiManager.addParameter(&custom_captive_user);
+    wifiManager.addParameter(&custom_captive_password);
+
 
     String ssid = "ESP" + String(ESP.getChipId());
 
@@ -159,6 +167,10 @@ void configPortal() {
     strcpy(mqtt_password, custom_mqtt_password.getValue());
     strcpy(mqtt_topic, custom_mqtt_topic.getValue());
     strcpy(blynk_token, custom_blynk_token.getValue());
+    strcpy(captive_user, custom_captive_user.getValue());
+    strcpy(captive_password, custom_captive_password.getValue());
+
+    
 
   //save the custom parameters to FS
   if (shouldSaveConfig) {
@@ -171,6 +183,8 @@ void configPortal() {
     json["mqtt_password"] = mqtt_password;
     json["mqtt_topic"] = mqtt_topic;
     json["blynk_token"] = blynk_token;
+    json["captive_user"] = captive_user;
+    json["captive_password"] = captive_password;
     
     File configFile = SPIFFS.open("/config.json", "w");
     if (!configFile) {
@@ -184,8 +198,100 @@ void configPortal() {
 }
 
 
+String urlencode(String str) {
+    String encodedString="";
+    char c;
+    char code0;
+    char code1;
+    char code2;
+    for (int i =0; i < str.length(); i++){
+      c=str.charAt(i);
+      if (c == ' '){
+        encodedString+= '+';
+      } else if (isalnum(c)){
+        encodedString+=c;
+      } else{
+        code1=(c & 0xf)+'0';
+        if ((c & 0xf) >9){
+            code1=(c & 0xf) - 10 + 'A';
+        }
+        c=(c>>4)&0xf;
+        code0=c+'0';
+        if (c > 9){
+            code0=c - 10 + 'A';
+        }
+        code2='\0';
+        encodedString+='%';
+        encodedString+=code0;
+        encodedString+=code1;
+        //encodedString+=code2;
+      }
+      yield();
+    }
+    return encodedString;
+}
+
+void authenticate() {
+  WiFiClientSecure client;
+  
+  while (!client.connect("7.7.7.7", 443)) {
+    Serial.println("connection failed");
+    delay(500);
+  }
+  
+  String url = "/login.html?redirect=go.microsoft.com/fwlink/?LinkID=219472&clcid=0x409";
+  String postParams = "buttonClicked=4&err_flag=0&err_msg=&info_flag=0&info_msg=&redirect_url=http%3A%2F%2Fgo.microsoft.com%2Ffwlink%2F%3FLinkID%3D219472%26clcid%3D0x409&network_name=Guest+Network&username=" +
+  urlencode(captive_user) + 
+  "&password=" +
+  urlencode(captive_password);
+
+  client.print(String("POST ") + url + " HTTP/1.1\r\n" +
+               "Host: 7.7.7.7\r\n" +
+               "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36\r\n" +
+               "Content-Type: application/x-www-form-urlencoded\r\n" +
+               "Connection: close\r\n"
+               "Content-Length: " + postParams.length() + "\r\n\r\n" +
+               postParams);
+
+  while (client.connected()) {
+    String line = client.readStringUntil('\n');
+    //Serial.println(line);
+  }
+}
+
+bool checkConnection() {
+      // Use WiFiClient class to create TCP connections
+    WiFiClient clienthttp;
+
+    if (!clienthttp.connect("gstatic.com", 80)) {
+        Serial.println("connection to http://gstatic.com failed");
+        Serial.println("wait 5 sec...");
+        delay(5000);
+        return false;
+    }
+
+      clienthttp.print(String("GET /generate_204 HTTP/1.1\r\n") +
+               "Host: gstatic.com\r\n" +
+               "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36\r\n" +
+               "Connection: close\r\n\r\n");
+
+  Serial.println("request sent to gstatic.com");
+  while (clienthttp.connected()) {
+    String line = clienthttp.readStringUntil('\n');
+    //Serial.println(line);
+    if (line.lastIndexOf("204 No Content") != -1) {
+      Serial.println("Connected to Internet");
+      return true;
+    }
+  }
+  Serial.println("No internet connection is available");
+  return false;
+
+}
+
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(TRIGGER_PIN, INPUT);
   start = millis();
 
@@ -198,6 +304,9 @@ void setup() {
 
   if (!isConfig) {
     establishWiFi();
+    if (!checkConnection()) {
+      authenticate();
+    }
     doMeasurement();
     sendBlynk();
     sendMqtt();
@@ -213,3 +322,4 @@ void setup() {
 void loop() {
   //do nothing in the loop
 }
+
